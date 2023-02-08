f4d4e89d6496fe5b68f33db429dfe29ffdc519f8
zhengwang <zhengwang@grandstream.cn>
2023-02-08 14:58:26 +0800

    create make symlink tool

---
+++ bin/make_symlink.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# source project 默认放在/data/project/{project_name} 目录下
+# 输出project默认放在/data/ide/{project_name}
+
+
+project=$1
+project_path="/data/project/$project"
+link_root="/data/ide/$project"
+
+
+if [ -z "$project" ];then
+    echo "please input project name"
+    exit 1
+fi
+
+if [ ! -d "$project_path" ];then
+    echo "$project_path not found, please check."
+    exit 1
+fi
+
+module_list=$(awk 'NF' /opt/aosp/etc/config)
+#echo "$path_list"
+
+
+rm -rf $link_root
+mkdir -p $link_root
+
+for module in ${module_list[@]}
+do
+    if [ -d $project_path/$module ];then
+        module_path=`dirname $link_root/$module`
+        mkdir -p  $module_path
+        ln -s $project_path/$module $module_path/.
+    else
+        echo "$project_path/$module not exist"
+    fi
+done
+
+echo "done, please check the project on $link_root"
+
